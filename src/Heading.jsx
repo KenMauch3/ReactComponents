6407bff1bc24c6dfe165f1645645ed00a5a1dc72
Ken Mauch <71083977+KenMauch3@users.noreply.github.com>
2021-09-06 11:35:30 -0400

    Initial commit

---
+++ src/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h1>My Favourite Foods</h1>
+      <p>in no particular order</p>
+    </div>
+  );
+}
+
+export default Heading;
